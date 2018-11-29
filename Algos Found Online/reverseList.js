88f915c71e7eb25647b9a058e48760939574e33c
Maximo DeLaRosa <mjd809@gmail.com>
2018-11-29 13:57:16 -0500

    Added some pseudo code

--- Algos Found Online/reverseList.js
+++ Algos Found Online/reverseList.js
@@ -1,6 +1,10 @@
 const reverseList = (head) => {
   let prev = null;
   let cur = head;
+  // Important to declare variable twice because It's advisable not to alter 
+  // the existing data
+  // cur.next needs to be iterated first before changing the value of tmp.next
+  // bc with objects manipulation it can change the value of head
   while (cur) {
     let tmp = cur;
     cur = cur.next;
