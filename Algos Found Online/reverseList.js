39b8e45a876f729865c220d8a52d16d8c701299d
Maximo DeLaRosa <mjd809@gmail.com>
2018-11-30 15:18:30 -0500

    Added more data to console log

--- Algos Found Online/reverseList.js
+++ Algos Found Online/reverseList.js
@@ -31,4 +31,19 @@ reverseList({
       }
     }
   }
-})
+}, {
+    "data": 1,
+    "next": {
+      "data": 2,
+      "next": {
+        "data": 3,
+        "next": {
+          "data": 4,
+          "next": {
+            "data": 5,
+            "next": null
+          }
+        }
+      }
+    }
+  })
