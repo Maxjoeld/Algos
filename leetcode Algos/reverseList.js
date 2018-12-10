cf347b09122e55f256d5d4ec402289e8f63fee24
Maximo DeLaRosa <mjd809@gmail.com>
2018-12-10 16:22:55 -0500

    Adds merged list

--- leetcode Algos/reverseList.js
+++ leetcode Algos/reverseList.js
@@ -14,6 +14,29 @@ const reverseList = (head) => {
   return prev;
 }
 
+// paste this into jsplaygrounds
+// we're able to iterate over an object like this 
+// because as we continues redclerating 
+// list we get further and further into the object
+// so everytime we're doing = list = list.next
+// we're techincally doing node.next.next.next = {}
+// let node = new ListNode();
+// let list = node;
+// node
+// list.next = {
+//   data: 4,
+//   next: null
+// };
+// node
+// list = list.next;
+
+// list.next = {
+//   data: 6,
+//   next: null
+// };
+// node
+
+
 
 
 reverseList({
