ea65b85eb36842a37df6198f5dbf974c2ceb8d0f
Maximo DeLaRosa <mjd809@gmail.com>
2018-12-19 09:37:16 -0500

    Adds buddyStrings

---
+++ leetcode Algos/buddystring.js
@@ -0,0 +1,15 @@
+var buddyStrings = function (A, B) {
+  if (A.length !== B.length) return false;
+  let noDups = new Set(A);
+  let swap = [];
+
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] !== B[i]) {
+      swap.push([A[i], B[i]]);
+    }
+  };
+  // There are multiple edge cases to consider in this problem
+  // One is what if there's multiple swaps in a string ? 
+  // which is why we explicitly check for the lengths in the first- second or condition 
+  return swap.length === 2 && swap[0].reverse().join('') === swap[1].join('') || (swap.length === 0 && noDups.size !== A.length);
+};
