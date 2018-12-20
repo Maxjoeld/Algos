74e985a2ca62841a24caf1b3d42dc0faa2f0ed64
Maximo DeLaRosa <mjd809@gmail.com>
2018-12-20 15:46:27 -0500

    Adds list of questions I have to review

---
+++ leetcode Algos/leetquest.js
@@ -0,0 +1,12 @@
+//Questions to go over again 
+// 665. Non - decreasing Array
+// 707 Design Linked List
+// 4 Median of Two Sorted Arrays 24.7 % Hard
+// 32 Longest Valid Parentheses
+// 3 Longest Substring Without Repeating Characters
+// 152 Maximum Product Subarray
+// 278 First bad version 
+// 168 Excel Sheet Column Title
+// 190 Reverse Bits
+// 28 Implement strStr()
+// 395 Longest Substring with At Least K Repeating Characters
