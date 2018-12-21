eb38751c5c59b8bcfea756c414e2b22b2489e419
Maximo <mjd809@gmail.com>
2018-12-21 11:38:12 -0500

    Adds longestpalindrome

---
+++ leetcode Algos/longestProductPalin.js
@@ -0,0 +1,25 @@
+var largestPalindrome = function(n) {
+  let max = Math.pow(10, n) -1;
+  let min = Math.pow(10, n-1);
+  let result = 0;
+  
+  for (let i = max; i >= min;i--) {
+      for (let j = max; j>= i; j--) {
+          let product = i * j;
+          // I tried doing !result instead of product > re3sult and it didnt work 
+          if (product > result) {
+              let revNum = String(i * j).split('').reverse().join('');
+              if (revNum ===  String(i * j)) {
+                  result = (i * j);
+                  break;
+              }
+          } else {
+              break;
+          }     
+      }
+      
+  }
+
+  return (result % 1337);
+};
+
