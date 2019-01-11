b49e738c2dea6936c040c8699d198988fce428fe
Maximo DeLaRosa <mjd809@gmail.com>
2019-01-10 19:23:09 -0500

    Adds quicksort

--- leetcode Algos/sortingAlgos/bubblesort.js
+++ leetcode Algos/sortingAlgos/bubblesort.js
@@ -1,14 +1,10 @@
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
-      let innerNum = arr[i];
-      let outterNum = arr[j];
-      if (j !== i) {
-        if (innerNum < outterNum) {
-          arr[j] = innerNum;
-          arr[i] = outterNum;
-          innerNum = arr[i + 1];
-        }
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
       }
     }
   }
--- leetcode Algos/sortingAlgos/mergesort.js
+++ leetcode Algos/sortingAlgos/mergesort.js
@@ -3,23 +3,21 @@ function mergeSort(arr) {
     return arr;
   }
 
-  const center = Math.floor(arr.length / 2);
-  const left = arr.slice(0, center);
-  const right = arr.slice(center);
+  const c = Math.floor(arr.length / 2);
+  const left = arr.slice(0, c);
+  const right = arr.slice(c);
 
   return merge(mergeSort(left), mergeSort(right));
 }
 
-function merge(left, right) {
-  const results = [];
-
-  while (left.length && right.length) {
-    if (left[0] < right[0]) {
-      results.push(left.shift());
-    } else {
-      results.push(right.shift());
-    }
-  }
-
-  return [...results, ...left, ...right];
+function merge(l, r) {
+   let res = [];
+   while (l.length && r.length) {
+     if (l[0] < r[0]) {
+       res.push(l.shift());
+     } else {
+       res.push(r.shift());
+     }
+   }
+   return [...res, ...r, ...l];
 }
